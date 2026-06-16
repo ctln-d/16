5bb4161dc96c33720514710c9350da7c5f00699c
ctln-d <caitlindu10@gmail.com>
2026-06-16 19:40:39 -0400

    edit favicon

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // any route that isn't an API route sends the frontend
 app.get('/{*path}', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(port, () => {
