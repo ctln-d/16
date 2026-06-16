bbb9ef8cdae78a902beeb975e5122e0749c3dc4b
ctln-d <caitlindu10@gmail.com>
2026-06-16 16:55:20 -0400

    style open pg

--- frontend/src/Open.jsx
+++ frontend/src/Open.jsx
@@ -1,7 +1,20 @@
-function Open() {
-    return (
-        <div>
+import back from "./assets/open-envelope.png";
+import front from "./assets/open-front.png";
+import invite from "./assets/invite.png";
+import { useState } from "react";
 
+function Open() {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div className="envelope">
+            <div className={`stack ${loaded ? "open" : ""}`}>
+                <img src={back} className="envelope-open" />
+                <div className={`window ${open ? "open" : ""}`}>
+                    <img src={invite} className="invite" />
+                </div>
+                <img src={front} className="envelope-front" />
+            </div>
         </div>
     )
 }
