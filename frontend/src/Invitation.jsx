2269cb4dd67e866e4ffcd45a22bde71faeaace37
ctln-d <caitlindu10@gmail.com>
2026-06-15 23:45:31 -0400

    pinterest board and second div

--- frontend/src/Invitation.jsx
+++ frontend/src/Invitation.jsx
@@ -1,28 +1,49 @@
 import invite from "./assets/invite.png";
 import "./Invitation.css";
 import { CgArrowLongDown } from "react-icons/cg";
-import { PinterestBoard } from "./components/PinterestBoard.jsx";
+import PinterestBoard from "./components/PinterestBoard.jsx";
 
 function Invitation() {
     return (
         <div>
-            <div className="invite-div">
+            <div className="white-div">
                 <img src={invite} className="invite-img" alt="invite" />
                 <CgArrowLongDown className="arrow"/>
             </div>
-            <div className="dress-div">
-                <p className="text">
+            <div className="yellow-div">
+                <p className="text" id="d">
                     inspired by the summer solstice and midsummer celebrations,<br/>
                     <br/>
                     you are encouraged to wear floor-length dresses or skirts;<br/>
                     <br/>
                     white, floral pastel, or vibrant, it doesn't matter!<br/>
-                    <PinterestBoard
-                        boardUrl="https://pin.it/2vwe5gIBn"
-                        boardWith={600}
-                        scaleHeight={500}
-                    />
                 </p>
+                <div className="pinterest-board">
+                    <PinterestBoard />
+                </div>
+            </div>
+            <div className="green-div">
+                <p id="c">we're going</p>
+                <div className="title-div">
+                    <p className="title" id="a">f</p>
+                    <p className="title" id="a">lower picking!</p>
+                </div>
+                <p className="address">
+                    Fancy Farms<br/>
+                    5204 Drane Field Rd.<br/>
+                    Lakeland, FL 33811
+                </p>
+                <p className="text">you may get dirt on you or it may rain, so bring a change of clothes/shoes if you want</p>
+                <p>then we're having</p>
+                <p className="title" id="b">dinner</p>
+                <p className="address">
+                    Cob & Pen<br/>
+                    1221 S. Florida Ave.<br/>
+                    Lakeland, FL 33803
+                </p>
+            </div>
+            <div className="yellow-div">
+
             </div>
         </div>
     )
--- frontend/src/components/PinterestBoard.jsx
+++ frontend/src/components/PinterestBoard.jsx
@@ -1,35 +1,24 @@
 import { useEffect } from "react";
 
-function PinterestBoard({ boardUrl, scaleWidth = 120, boardWith = 400, scaleHeight = 400 }) {
+function PinterestBoard() {
     useEffect(() => {
-        const scriptId = "pinit-script";
-        let script = document.getElementById(scriptId);
-
-        if (!script) {
-            script = document.createElement("script");
-            script.id = scriptId;
-            script.async = true;
-            script.defer = true;
-            script.src = "https://pin.it/2vwe5gIBn";
-            document.body.appendChild(script);
-        } else if (window.doBuild) {
-            window.doBuild();
-        }
-    }, [boardUrl])
+        const script = document.createElement("script");
+        script.src = "https://assets.pinterest.com/js/pinit.js";
+        script.async = true;
+        document.body.appendChild(script);
+    }, []);
 
     return (
-        <div className="pin-board-container">
-            <a
-                data-pin-do="embedBoard"
-                data-pin-scale-width={scaleWidth}
-                data-pin-scale-height={scaleHeight}
-                data-pin-board-width={boardWidth}
-                href={boardUrl}
-            >
-                View board on Pinterest
-            </a>
-        </div>
-    )
+        <a
+            data-pin-do="embedBoard"
+            data-pin-board-width="400"
+            data-pin-scale-height="240"
+            data-pin-scale-width="80"
+            href="https://www.pinterest.com/lovuqq/inv/"
+        >
+            view on pinterest
+        </a>
+    );
 }
 
-export { PinterestBoard };
+export default PinterestBoard;
